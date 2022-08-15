2ecd5f25bc27c912519939b48b49eec0bb55c52d
Łukasz Piwowar <zsacul@gmail.com>
2022-08-16 00:05:16 +0200

    r1

---
+++ src/Dydaktyka.js
@@ -0,0 +1,238 @@
+import React from 'react';
+import './App.css';
+import './index.js';
+
+class Dydaktyka extends React.Component {
+  componentDidMount () {
+  }
+
+render() {
+  return (
+
+            <article>
+            <div className='content'>
+<br />
+            <h3>Semestr letni 2021/2022</h3>
+            <li>Wykład - Kurs modelowania 3d i wizualizacji w programie SketchUp</li>
+            <li>Pracownia - Kurs modelowania 3d i wizualizacji w programie SketchUp</li>
+            <li>Pracownia - Kurs modelowania 3d i wizualizacji w programie SketchUp</li>
+<br />
+            <h3>Semestr zimowy 2021/2022</h3>
+            <li>Lecture - Unity game development course</li>
+            <li>Lab - Unity game development course</li>
+            <li>Lab - Unity game development course</li>
+            <li>Pracownia - Kurs: Wstęp do programowania w języku C</li>
+            <li>Wykład - Kurs języka Rust</li>
+            <li>Pracownia - Kurs języka Rust</li>
+            <li>Pracownia - Kurs języka Rust</li>
+<br />
+            <h3>Semestr letni 2020/2021</h3>
+            <li>Wykład - Kurs modelowania 3d i wizualizacji w programie SketchUp</li>
+            <li>Pracownia - Kurs modelowania 3d i wizualizacji w programie SketchUp</li>
+            <li>Pracownia - Kurs modelowania 3d i wizualizacji w programie SketchUp</li>
+            <li>Pracownia - Minikurs języka C</li>
+<br />
+            <h3>Semestr zimowy 2020/2021</h3>
+            <li>Wykład - Kurs języka Rust</li>
+            <li>Pracownia - Kurs języka Rust</li>
+            <li>Wykład - Kurs tworzenia gier w silniku Unity3D</li>
+            <li>Pracownia - Kurs tworzenia gier w silniku Unity3D</li>
+            <li>Pracownia - Kurs tworzenia gier w silniku Unity3D</li>
+            <li>Pracownia - Kurs: Wstęp do programowania w języku C</li>
+<br />
+            <h3>Semestr letni 2019/2020</h3>            
+            <li>Wykład - Kurs modelowania 3d i wizualizacji w programie SketchUp</li>
+            <li>Pracownia - Kurs modelowania 3d i wizualizacji w programie SketchUp</li>
+            <li>Seminarium - Projekt zespołowy: silnik Unity3D i wirtualna rzeczywistość LATO</li>
+            <li>Pracownia - Projekt zespołowy: silnik Unity3D i wirtualna rzeczywistość LATO</li>
+<br />
+            <h3>Semestr zimowy 2018/2019</h3>
+            <li>Wykład - Kurs tworzenia gier w silniku Unity3D</li>
+            <li>Pracownia - Kurs tworzenia gier w silniku Unity3D</li>
+            <li>Wykład - Kurs języka Rust</li>
+            <li>Pracownia - Kurs języka Rust</li>
+            <li>Pracownia - Kurs języka Rust</li>
+<br />
+            <h3>Semestr letni 2018/2019</h3>
+            <li>Wykład - Kurs modelowania 3d i wizualizacji w programie SketchUp</li>
+            <li>Pracownia 1 - Kurs modelowania 3d i wizualizacji w programie SketchUp</li>
+            <li>Pracownia 2 - Kurs modelowania 3d i wizualizacji w programie SketchUp</li>
+            <li>Pracownia 3 - Kurs modelowania 3d i wizualizacji w programie SketchUp</li>
+<br />
+            <h3>Semestr zimowy 2018/2019</h3>
+            <li>Wykład - Kurs tworzenia gier w silniku Unity3D</li>
+            <li>Pracownia 1 - Kurs tworzenia gier w silniku Unity3D</li>
+            <li>Pracownia 2 - Kurs tworzenia gier w silniku Unity3D</li>
+<br />
+            <h3>Semestr letni 2017/2018</h3>
+            <li>Wykład - Kurs modelowania 3d i wizualizacji w programie SketchUp</li>
+            <li>Pracownia - Kurs modelowania 3d i wizualizacji w programie SketchUp</li>
+            <li>Pracownia - Kurs modelowania 3d i wizualizacji w programie SketchUp</li>
+            <li>Seminarium - Projekt zespołowy: silnik Unity3D i wirtualna rzeczywistość LATO</li>
+            <li>Pracownia - Projekt zespołowy: silnik Unity3D i wirtualna rzeczywistość LATO</li>
+<br />
+            <h3>Semestr zimowy 2017/2018</h3>
+            <li>Wykład - Kurs tworzenia gier w silniku Unity3D</li>
+            <li>Pracownia - Kurs tworzenia gier w silniku Unity3D</li>
+            <li>Pracownia - Kurs tworzenia gier w silniku Unity3D</li>
+            <li>Pracownia - Systemy wbudowane</li>
+<br />
+            <h3>Semestr letni 2016/2017</h3>
+            <li>Wykład - Kurs modelowania 3d i wizualizacji w programie SketchUp</li>
+            <li>Pracownia - Kurs modelowania 3d i wizualizacji w programie SketchUp</li>
+            <li>Wykład - Projekt zespołowy: Silnik Unity3D i wirtualna rzeczywistość 2</li>
+            <li>Pracownia - Projekt zespołowy: Silnik Unity3D i wirtualna rzeczywistość 2</li>
+            <li>Kurs programowania pod Windows w technologii .NET</li>
+<br />
+            <h3>Semestr zimowy 2016/2017</h3>
+            <li>Wstęp do programowania w jezyku C</li>
+            <li>Wykład - Systemy wbudowane</li>
+            <li>Pracownia - Systemy wbudowane</li>
+            <li>Pracownia - Systemy wbudowane</li>
+            <li>Seminarium - Silnik Unity i wirtualna rzeczywistość</li>
+<br />
+            <h3>Semestr letni 2015/2016</h3>
+            <li>Wykład - Kurs modelowania 3d i wizualizacji w programie SketchUp</li>
+            <li>Pracownia - Kurs modelowania 3d i wizualizacji w programie SketchUp</li>
+            <li>Kurs programowania pod Windows w technologii .NET</li>
+            <li>Kurs C++</li>
+<br />
+            <h3>Semestr zimowy 2015/2016</h3>
+            <li>Wstęp do programowania w jezyku C</li>
+            <li>Wykład - Systemy wbudowane</li>
+            <li>Pracownia - Systemy wbudowane</li>
+            <li>Pracownia - Systemy wbudowane</li>
+<br />
+            <h3>Semestr letni 2014/2015</h3>
+            <li>Wykład - Kurs modelowania 3d i wizualizacji w programie SketchUp</li>
+            <li>Pracownia - Kurs modelowania 3d i wizualizacji w programie SketchUp</li>
+            <li>Pracownia - Kurs modelowania 3d i wizualizacji w programie SketchUp</li>
+<br />
+            <h3>Semestr zimowy 2014/2015</h3>
+            <li>Pracownia - Podstawy grafiki komputerowej 1</li>
+            <li>Pracownia - Podstawy grafiki komputerowej 2</li>
+            <li>Wykład - Systemy wbudowane</li>
+            <li>Pracownia - Systemy wbudowane</li>
+            <li>Pracownia - Systemy wbudowane</li>
+<br />
+            <h3>Semestr letni 2013/2014</h3>
+            <li>Kurs programowania pod Windows w technologii .NET (ang.)</li>
+            <li>Kurs programowania pod Windows w technologii .NET (grupa 1)</li>
+            <li>Kurs programowania pod Windows w technologii .NET (grupa 2)</li>
+<br />
+            <h3>Semestr zimowy 2013/2014</h3>
+            <li>Pracownia - Podstawy grafiki komputerowej 1</li>
+            <li>Pracownia - Podstawy grafiki komputerowej 2</li>
+            <li>Wykład - Systemy wbudowane</li>
+            <li>Pracownia - Systemy wbudowane</li>
+            <li>Pracownia - Systemy wbudowane</li>
+<br />
+            <h3>Semestr letni 2012/2013</h3>
+            <li>Kurs programowania pod Windows w technologii .NET</li>
+            <li>Kurs C++</li>
+<br />
+            <h3>Semestr zimowy 2012/2013</h3>
+            <li>Pracownia - Kurs ANSI C z elementami C++</li>
+            <li>Pracownia - Kurs WWW</li>
+            <li>Pracownia - Podstawy grafiki komputerowej 1</li>
+            <li>Pracownia - Podstawy grafiki komputerowej 2</li>
+            <li>Wykład - Systemy wbudowane</li>
+            <li>Pracownia - Systemy wbudowane</li>
+            <li>Pracownia - Systemy wbudowane</li>
+<br />
+            <h3>Semestr letni 2011/2012</h3>
+            <li>Kurs programowania pod Windows w technologii .NET (grupa I)</li>
+            <li>Kurs programowania pod Windows w technologii .NET (grupa II)</li>
+            <li>Kurs C++</li>
+<br />
+            <h3>Semestr zimowy 2011/2012</h3>
+            <li>Pracownia - Kurs ANSI C z elementami C++</li>
+            <li>Pracownia - Kurs WWW</li>
+            <li>Pracownia - Podstawy grafiki komputerowej</li>
+            <li>Wykład - Systemy wbudowane</li>
+            <li>Pracownia - Systemy wbudowane</li>
+<br />
+            <h3>Semestr letni 2010/2011</h3>
+            <li>Kurs programowania pod Windows w technologii .NET</li>
+            <li>Kurs WWW</li>
+            <li>C++ course (in English)</li>
+            <li>Kurs C++</li>
+<br />
+            <h3>Semestr zimowy 2010/2011</h3>
+            <li>Pracownia - Procesory graficzne w obliczeniach równoległych (CUDA) (grupa 1)</li>
+            <li>Pracownia - Procesory graficzne w obliczeniach równoległych (CUDA) (grupa 2)</li>
+            <li>Pracownia - Kurs ANSI C z elementami C++ (grupa 1)</li>
+            <li>Pracownia - Kurs ANSI C z elementami C++ (grupa 2)</li>
+            <li>Pracownia - Kurs języka java</li>
+            <li>Laboratorium - Wstęp do informatyki i programowania w IM UWr (WIP)</li>
+<br />
+            <h3>Semestr letni 2009/2010</h3>
+            <li>Kurs programowania pod Windows w technologii .NET</li>
+            <li>Kurs WWW</li>
+            <li>Advanced C++ course (in English)</li>
+<br />
+            <h3>Semestr zimowy 2009/2010</h3>
+            <li>Trendy w IT z Google</li>
+            <li>Pracownia - Kurs ANSI C z elementami C++</li>
+            <li>Pracownia - Podstawy grafiki komputerowej</li>
+            <li>Pracownia - Kurs języka java</li>
+            <li>Laboratorium - Wstęp do informatyki i programowania w IM UWr (WIP)</li>
+<br />
+            <h3>Semestr letni 2008/2009</h3>
+            <li>Kurs programowania pod Windows w technologii .NET</li>
+            <li>Licencjacki projekt programistyczny</li>
+<br />
+            <h3>Semestr zimowy 2008/2009</h3>
+            <li>Pracownia - Kurs ANSI C z elementami C++ (grupa 1)</li>
+            <li>Pracownia - Kurs ANSI C z elementami C++ (grupa 2)</li>
+            <li>Pracownia - Podstawy grafiki komputerowej</li>
+            <li>Pracownia - Kurs języka java</li>
+            <li>Laboratorium - Wstęp do informatyki i programowania w IM UWr (WIP)</li>
+<br />
+            <h3>Semestr letni 2007/2008</h3>
+            <li>Kurs programowania pod Windows w technologii .NET</li>
+<br />
+            <h3>Semestr zimowy 2007/2008</h3>
+            <li>Pracownia - Kurs ANSI C z elementami C++</li>
+            <li>Pracownia - Podstawy grafiki komputerowej</li>
+<br />
+            <h3>Semestr letni 2006/2007</h3>
+            <li>Ćwiczenia + pracownia - Programowanie obiektowe</li>
+<br />
+            <h3>Semestr zimowy 2006/2007</h3>
+            <li>Pracownia - Kurs ANSI C z elementami C++</li>
+            <li>Pracownia - Algorytmy i struktury danych (lic)</li>
+<br />
+            <h3>Semestr letni 2005/2006</h3>
+            <li>Pracownia - Programowanie pod Windows.NET</li>
+            <li>Pracownia - Programowanie obiektowe</li>
+<br />
+            <h3>Semestr zimowy 2005/2006</h3>
+            <li>Pracownia - Kurs języka C (studia wieczorowe)</li>
+            <li>Pracownia - Podstawy grafiki komputerowej</li>
+<br />
+            <h3>Semestr letni 2004/2005</h3>
+            <li>Seminarium licencjackie (projekt programistyczny - studia wieczorowe)</li>
+<br />
+            <h3>Semestr zimowy 2004/2005</h3>
+            <li>Pracownia - Kurs języka C (studia wieczorowe)</li>
+            <li>Pracownia - Kurs języka java</li>
+            <li>Pracownia - Podstawy grafiki komputerowej</li>
+<br />
+            <h3>Semestr letni 2003/2004</h3>
+            <li>Pracownia - Realistyczna Grafika Komputerowa</li>
+            <li>Pracownia - Kurs WWW - czyli opowieści niesamoWWWite</li>
+            <li>Seminarium licencjackie (projekt programistyczny - studia wieczorowe)</li>
+<br />
+            <h3>Semestr zimowy 2003/2004</h3>
+            <li>Pracownia - Kurs języka C (studia wieczorowe)</li>
+            <li>Pracownia - Kurs języka ANSI C (z elementami C++)</li>
+            <li>Pracownia - Podstawy grafiki komputerowej</li>
+<br />
+            </div>
+            </article>
+  );
+}
+
+}
+export default Dydaktyka;
